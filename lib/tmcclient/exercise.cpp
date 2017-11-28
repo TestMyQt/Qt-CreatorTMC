462b2a742a8de197e49ed0481306bc2d07b815fe
Henrik Lindberg <morgulcore@gmail.com>
2017-11-28 19:07:25 +0200

    Update code of class Exercise from master
    Used the simple or simplistic approach of copy-pasting the newest
    version of the class from master and then copy-pasting the QDoc
    to it.

--- lib/tmcclient/exercise.cpp
+++ lib/tmcclient/exercise.cpp
@@ -19,41 +19,95 @@ Exercise::Exercise(int id, QString name)
 }
 
 /*! Getter function for the exercise ID. */
-int Exercise::getId() const {
+int Exercise::getId() const
+{
     return m_id;
 }
 
 /*! Getter function for the exercise name. */
-QString Exercise::getName() const {
+QString Exercise::getName() const
+{
     return m_name;
 }
 
 /*! Getter function for the directory location of the exercise. */
-QString Exercise::getLocation() const {
+QString Exercise::getLocation() const
+{
     return m_location;
 }
 
-// TODO: Add QDoc comment
-QString Exercise::getChecksum() const {
+QString Exercise::getChecksum() const
+{
     return m_checksum;
 }
 
+QString Exercise::getDlDate() const
+{
+    return m_dlDate;
+}
+
+bool Exercise::getOpenStatus() const
+{
+    return m_openStatus;
+}
+
 /*! Sets the exercise ID to \a id. */
-void Exercise::setId(int id) {
+void Exercise::setId(int id)
+{
     m_id = id;
 }
 
 /*! Sets the exercise name to \a name. */
-void Exercise::setName(QString name) {
+void Exercise::setName(QString name)
+{
     m_name = name;
 }
 
 /*! Sets the directory location of the exercise to \a location. */
-void Exercise::setLocation(QString location) {
+void Exercise::setLocation(QString location)
+{
     m_location = location;
 }
 
-// TODO: Add QDoc comment
-void Exercise::setChecksum(QString checksum) {
+void Exercise::setChecksum(QString checksum)
+{
     m_checksum = checksum;
+}
+
+void Exercise::setDlDate(QString dlDate)
+{
+    m_dlDate = dlDate;
+}
+
+void Exercise::setOpenStatus(bool openStatus)
+{
+    m_openStatus = openStatus;
+}
+
+void Exercise::saveSettings(QString courseName)
+{
+    // Yay or nay on the indentation?
+    QSettings settings("TestMyQt", "Exercises");
+    settings.beginGroup(courseName);
+        settings.beginGroup(m_name);
+            settings.setValue("id", m_id);
+            settings.setValue("checksum", m_checksum);
+            settings.setValue("location", m_location);
+            settings.setValue("dlDate", m_dlDate);
+            settings.setValue("openStatus", m_openStatus);
+        settings.endGroup();
+    settings.endGroup();
+    settings.deleteLater();
+}
+
+Exercise Exercise::fromQSettings(QSettings *settings)
+{
+    Exercise ex = Exercise(settings->value("id").toInt(),
+                           settings->value("name").toString());
+
+    ex.setChecksum(settings->value("checksum", "").toString());
+    ex.setLocation(settings->value("location", "").toString());
+    ex.setDlDate(settings->value("dlDate", "").toString());
+    ex.setOpenStatus(settings->value("openStatus", false).toBool());
+    return ex;
 }
