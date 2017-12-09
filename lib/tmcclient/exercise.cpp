07ea7fa30eb1f104acc08683b133be9f1b3d273a
Henrik Lindberg <henrik.lindberg@cs.helsinki.fi>
2017-12-09 20:27:15 +0200

    Complete QDoc of class Exercise

--- lib/tmcclient/exercise.cpp
+++ lib/tmcclient/exercise.cpp
@@ -5,9 +5,11 @@
     \brief \l Exercise objects represent TMC course exercises.
 
     In practice TMC exercises are directories that contain the files of a miniscule
-    software project. One of the instance variables of \l Exercise is one that
-    specifies its location. The location is the parent directory of the exercise
-    directory itself.
+    software project. One of the fields of class \l Exercise is one that specifies its
+    location. The location is the parent directory of the exercise directory itself.
+    The location should be named after the name of the course the exercise belongs to.
+    For example, if the course name is Foo and the exercise is called Bar, then the
+    exercise path should look something like \c {~/tmc/Foo/Bar}.
 */
 
 #include "exercise.h"
@@ -30,12 +32,26 @@ Exercise::Exercise(int id, QString name)
     m_unzipped = false;
 }
 
+/*!
+    Returns \c true if the \l Exercise object is equal to the parameter \a other;
+    otherwise returns \c false. The overloaded operator expression \c {e1 == e2}
+    evaluates to \c true when both the \l {Exercise::getId()} {id} and
+    \l {Exercise::getChecksum()} {checksum} fields of the \l Exercise objects
+    \c e1 and \c e2 are equal; otherwise the expression evaluates to \c false.
+*/
 bool Exercise::operator==(const Exercise &other) const
 {
     return other.getId() == m_id &&
             other.getChecksum() == m_checksum;
 }
 
+/*!
+    Returns \c true if the \l Exercise object is distinct from parameter \a other;
+    otherwise returns \c false. The overloaded operator expression \c {e1 != e2}
+    evaluates to \c true when either the \l {Exercise::getId()} {id} or
+    \l {Exercise::getChecksum()} {checksum} fields of the operands are not equal;
+    otherwise the expression evaluates to \c false.
+*/
 bool Exercise::operator!=(const Exercise &other) const
 {
     return !(*this == other);
@@ -67,9 +83,6 @@ QString Exercise::getLocation() const
 
 /*!
     Getter function for the checksum field of the \l Exercise object.
-
-    \note See \l {Exercise::} {setChecksum()} for a short description of
-    TMC exercise checksums.
 */
 QString Exercise::getChecksum() const
 {
@@ -88,19 +101,31 @@ QString Exercise::getDlDate() const
     Getter function for the open status of the \l Exercise object.
     If the corresponding TMC exercise project has been opened in Qt Creator,
     \c true is returned and \c false otherwise.
-
-    \b{TODO:} Do fact-check!
 */
 bool Exercise::getOpenStatus() const
 {
     return m_openStatus;
 }
 
+/*!
+    Returns \c true if the TMC exercise corresponding to the \l Exercise object
+    has been downloaded; otherwise returns false.
+
+    \note Before downloading an actual TMC exercise (which comes in the form of
+    a compressed archive) a list of available TMC exercises is downloaded. The
+    exercises to be downloaded are selected from this list and are marked as
+    downloaded only after a successful download operation of the exercise
+    archive files.
+*/
 bool Exercise::isDownloaded() const
 {
     return m_downloaded;
 }
 
+/*!
+    Returns \c true if the archive file corresponding to the \l Exercise object
+    has already been extracted; otherwise returns \c false.
+*/
 bool Exercise::isUnzipped() const
 {
     return m_unzipped;
@@ -133,8 +158,6 @@ void Exercise::setLocation(QString location)
 /*!
     Sets the TMC exercise checksum field of the \l Exercise object
     to \a checksum.
-
-    \b{TODO:} Add a short description of TMC exercise checksums.
 */
 void Exercise::setChecksum(QString checksum)
 {
@@ -157,28 +180,38 @@ void Exercise::setDlDate(QString dlDate)
     status should be set to \c true when the corresponding TMC exercise project
     is opened in Qt Creator; when the project is closed, the open status should
     be set to \c false.
-
-    \b{TODO:} Do fact-check!
 */
 void Exercise::setOpenStatus(bool openStatus)
 {
     m_openStatus = openStatus;
 }
 
+/*!
+    Sets the download status of the \l Exercise object to the value of parameter
+    \a downloaded.
+*/
 void Exercise::setDownloaded(bool downloaded)
 {
     m_downloaded = downloaded;
 }
 
+/*!
+    After being downloaded a TMC exercise is in memory in the form
+    of a compressed (zip) archive. The obvious next step is to extract the
+    archive and save the resulting directory in the local file system. After
+    the extraction process has been completed \c {setUnzipped()} of the
+    corresponding \l Exercise object is called with the parameter \a zipped
+    set to \c true.
+*/
 void Exercise::setUnzipped(bool zipped)
 {
     m_unzipped = zipped;
 }
 
 /*!
-    Saves the \l Exercise object's state in persistent storage using
-    \l {http://doc.qt.io/qt-5/qsettings.html} {QSettings}. The \l
-    {http://doc.qt.io/qt-5/qstring.html} {QString} parameter \a courseName
+    Saves the \l Exercise object's state in persistent storage using the
+    \l {http://doc.qt.io/qt-5/qsettings.html} {QSettings} parameter \a settings.
+    The \l {http://doc.qt.io/qt-5/qstring.html} {QString} parameter \a courseName
     is used as a prefix in forming the key names with \l
     {http://doc.qt.io/qt-5/qsettings.html#beginGroup} {QSettings::beginGroup()}.
 */
@@ -205,8 +238,8 @@ void Exercise::saveQSettings(QSettings *settings, const QString courseName)
     should be present in \a settings.
 
     \note It is assumed that the appropriate prefix has been set for the \a settings
-    parameter in the calling function using the \l
-    {http://doc.qt.io/qt-5/qsettings.html#beginGroup} {beginGroup()} function.
+    parameter in the calling function using
+    \l {http://doc.qt.io/qt-5/qsettings.html#beginGroup} {QSettings::beginGroup()}.
  */
 Exercise Exercise::fromQSettings(QSettings *settings)
 {
@@ -222,9 +255,16 @@ Exercise Exercise::fromQSettings(QSettings *settings)
     return ex;
 }
 
+/*!
+    Uses the \l {http://doc.qt.io/qt-5/qjsonobject.html} {QJsonObject}
+    parameter \a jsonExercise to initialize and return a new \l Exercise object.
+    Initializing the \l Exercise object involves setting the \l {Exercise::getName()}
+    {name}, \l {Exercise::getId()} {id}, \l {Exercise::getChecksum()} {checksum}
+    and \l {Exercise::getDlDate()} {deadline} fields to the values extracted from
+    \a jsonExercise.
+ */
 Exercise Exercise::fromJson(const QJsonObject jsonExercise)
 {
-
     Exercise fromJson = Exercise(jsonExercise["id"].toInt(),
             jsonExercise["name"].toString());
     fromJson.setChecksum(jsonExercise["checksum"].toString());
